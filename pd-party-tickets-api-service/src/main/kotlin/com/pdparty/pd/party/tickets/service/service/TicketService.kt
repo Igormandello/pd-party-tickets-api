b2b8e01c6a5c8fdd3bb8c98c33503724c3c3b598
Igormandello <igormandello@gmail.com>
2019-03-06 03:22:14 -0300

    Implements the ticket service

--- pd-party-tickets-api-service/src/main/kotlin/com/pdparty/pd/party/tickets/service/service/TicketService.kt
+++ pd-party-tickets-api-service/src/main/kotlin/com/pdparty/pd/party/tickets/service/service/TicketService.kt
@@ -4,11 +4,23 @@ import com.pdparty.pd.party.tickets.service.dao.TicketDAO
 import com.pdparty.pd.party.tickets.service.model.Ticket
 
 class TicketService(val ticketDao: TicketDAO) {
-  suspend fun insertTicket(ticket: Ticket) {}
+  suspend fun insertTicket(ticket: Ticket) {
+    require(this.find(ticket.id) == null) { "A ticket with that ID already exists." }
 
-  suspend fun updateTicket(ticket: Ticket) {}
+    ticketDao.insert(ticket)
+  }
 
-  suspend fun deleteTicket(ticketId: String) {}
+  suspend fun updateTicket(ticket: Ticket) {
+    require(this.find(ticket.id) != null) { "Can't update a non existing ticket." }
+
+    ticketDao.update(ticket)
+  }
+
+  suspend fun deleteTicket(ticketId: String) {
+    require(this.find(ticketId) != null) { "Can't delete a non existing ticket." }
+
+    ticketDao.delete(ticketId)
+  }
 
   suspend fun find(ticketId: String): Ticket? =
     ticketDao.find(ticketId)
