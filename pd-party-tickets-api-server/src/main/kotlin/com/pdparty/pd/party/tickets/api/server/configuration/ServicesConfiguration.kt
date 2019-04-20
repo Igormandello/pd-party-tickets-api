6db557d5626ee1b0173d674caf0be006d0f5f702
Igormandello <igormandello@gmail.com>
2019-04-20 03:29:10 -0300

    Fixes missing parameter in ServicesConfiguration

--- pd-party-tickets-api-server/src/main/kotlin/com/pdparty/pd/party/tickets/api/server/configuration/ServicesConfiguration.kt
+++ pd-party-tickets-api-server/src/main/kotlin/com/pdparty/pd/party/tickets/api/server/configuration/ServicesConfiguration.kt
@@ -2,9 +2,11 @@ package com.pdparty.pd.party.tickets.api.server.configuration
 
 import com.pdparty.pd.party.tickets.service.`in`.memory.InMemoryTicketDAO
 import com.pdparty.pd.party.tickets.service.`in`.memory.InMemoryUserDAO
+import com.pdparty.pd.party.tickets.service.model.User
 import com.pdparty.pd.party.tickets.service.service.TicketService
 import com.pdparty.pd.party.tickets.service.service.UserService
 import com.pdparty.pd.party.tickets.service.service.security.AuthenticationService
+import com.pdparty.pd.party.tickets.service.service.security.SecurityContext
 import com.pdparty.pd.party.tickets.service.service.security.SignupRequest
 import com.typesafe.config.ConfigFactory
 import kotlinx.coroutines.runBlocking
@@ -26,7 +28,8 @@ object ServicesConfiguration {
         SignupRequest(
           adminConfig.getString("username"),
           adminConfig.getString("password")
-        )
+        ),
+        SecurityContext(User("root", "", ""))
       )
     }
   }
